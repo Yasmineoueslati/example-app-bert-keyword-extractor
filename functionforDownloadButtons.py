9082aec352993d919455c315348ffdb067fadd90
Yasmine <73404550+Yasmineoueslati@users.noreply.github.com>
2022-11-09 17:50:38 +0100

    Update functionforDownloadButtons.py

--- functionforDownloadButtons.py
+++ functionforDownloadButtons.py
@@ -1,171 +1,114 @@
 import streamlit as st
-import pickle
-import pandas as pd
+import numpy as np
+from pandas import DataFrame
+from keybert import KeyBERT
+# For Flair (Keybert)
+import seaborn as sns
+# For download buttons
+from functionforDownloadButtons import download_button
+import os
 import json
-import base64
-import uuid
-import re
 
-import importlib.util
-
-
-def import_from_file(module_name: str, filepath: str):
-    """
-    Imports a module from file.
-
-    Args:
-        module_name (str): Assigned to the module's __name__ parameter (does not
-            influence how the module is named outside of this function)
-        filepath (str): Path to the .py file
-
-    Returns:
-        The module
-    """
-    spec = importlib.util.spec_from_file_location(module_name, filepath)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+st.set_page_config(
+    page_title="Review Analyzer |Topic Modeling",
+    page_icon="🎈",
+)
 
 
-def notebook_header(text):
-    """
-    Insert section header into a jinja file, formatted as notebook cell.
-
-    Leave 2 blank lines before the header.
-    """
-    return f"""# # {text}
-
-"""
-
-
-def code_header(text):
-    """
-    Insert section header into a jinja file, formatted as Python comment.
-
-    Leave 2 blank lines before the header.
-    """
-    seperator_len = (75 - len(text)) / 2
-    seperator_len_left = math.floor(seperator_len)
-    seperator_len_right = math.ceil(seperator_len)
-    return f"# {'-' * seperator_len_left} {text} {'-' * seperator_len_right}"
-
-
-def to_notebook(code):
-    """Converts Python code to Jupyter notebook format."""
-    notebook = jupytext.reads(code, fmt="py")
-    return jupytext.writes(notebook, fmt="ipynb")
-
-
-def open_link(url, new_tab=True):
-    """Dirty hack to open a new web page with a streamlit button."""
-    # From: https://discuss.streamlit.io/t/how-to-link-a-button-to-a-webpage/1661/3
-    if new_tab:
-        js = f"window.open('{url}')"  # New tab or window
-    else:
-        js = f"window.location.href = '{url}'"  # Current tab
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    st.bokeh_chart(div)
-
-
-def download_button(object_to_download, download_filename, button_text):
-    """
-    Generates a link to download the given object_to_download.
-
-    From: https://discuss.streamlit.io/t/a-download-button-with-custom-css/4220
-
-    Params:
-    ------
-    object_to_download:  The object to be downloaded.
-    download_filename (str): filename and extension of file. e.g. mydata.csv,
-    some_txt_output.txt download_link_text (str): Text to display for download
-    link.
-
-    button_text (str): Text to display on download button (e.g. 'click here to download file')
-    pickle_it (bool): If True, pickle file.
-
-    Returns:
-    -------
-    (str): the anchor tag to download object_to_download
-
-    Examples:
-    --------
-    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
-    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
-
-    """
-    # if pickle_it:
-    #    try:
-    #        object_to_download = pickle.dumps(object_to_download)
-    #    except pickle.PicklingError as e:
-    #        st.write(e)
-    #        return None
-
-    # if:
-    if isinstance(object_to_download, bytes):
-        pass
-
-    elif isinstance(object_to_download, pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=False)
-    # Try JSON encode for everything else
-    else:
-        object_to_download = json.dumps(object_to_download)
-
-    try:
-        # some strings <-> bytes conversions necessary here
-        b64 = base64.b64encode(object_to_download.encode()).decode()
-    except AttributeError as e:
-        b64 = base64.b64encode(object_to_download).decode()
-
-    button_uuid = str(uuid.uuid4()).replace("-", "")
-    button_id = re.sub("\d+", "", button_uuid)
-
-    custom_css = f""" 
-        <style>
-            #{button_id} {{
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
-                background-color: rgb(255, 255, 255);
-                color: rgb(38, 39, 48);
-                padding: .25rem .75rem;
-                position: relative;
-                text-decoration: none;
-                border-radius: 4px;
-                border-width: 1px;
-                border-style: solid;
-                border-color: rgb(230, 234, 241);
-                border-image: initial;
-            }} 
-            #{button_id}:hover {{
-                border-color: rgb(246, 51, 102);
-                color: rgb(246, 51, 102);
-            }}
-            #{button_id}:active {{
-                box-shadow: none;
-                background-color: rgb(246, 51, 102);
-                color: white;
-                }}
-        </style> """
-
-    dl_link = (
-        custom_css
-        + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br><br>'
+def _max_width_():
+    max_width_str = f"max-width: 1800px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
     )
-    # dl_link = f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}"><input type="button" kind="primary" value="{button_text}"></a><br></br>'
 
-    st.markdown(dl_link, unsafe_allow_html=True)
+_max_width_()
+
+c30, c31, c32 = st.columns([2.5, 1, 3])
+
+with c30:
+    # st.image("logo.png", width=400)
+    st.title("🔑 Review Analyzer")
+    st.header("")
+
+with st.expander("ℹ️ - About this app", expanded=True):
+    st.write(
+        """     
+-   Cette Application est pour le Topic Modeling 
+- Ecrire votre avis et regarde la magie ! 	    """
+    )
+
+    st.markdown("")
+
+st.markdown("")
+st.markdown("## **📌 Paste document **")
+with st.form(key="my_form"):
+    ce, c1, ce, c2, c3 = st.columns([0.07, 1, 0.07, 5, 0.07])
+    with c1:
+        ModelType = st.radio(
+            "Quel texte analyser ?",
+            ["Avis dataset", "Texte libre"],
+            help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
+        )
 
 
-# def download_link(
-#     content, label="Download", filename="file.txt", mimetype="text/plain"
-# ):
-#     """Create a HTML link to download a string as a file."""
-#     # From: https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/9
-#     b64 = base64.b64encode(
-#         content.encode()
-#     ).decode()  # some strings <-> bytes conversions necessary here
-#     href = (
-#         f'<a href="data:{mimetype};base64,{b64}" download="{filename}">{label}</a>'
-#     )
-#     return href
+
+        top_N = st.slider(
+            "Mettez le nombre de Topic souhaité",
+            min_value=1,
+            max_value=15,
+            value=10,
+            help="You can choose the number that you want between 1 and 15.",
+        )
+
+    with c2:
+        doc = st.text_area(
+            "Paste your text below (max 500 words)",
+            height=510,
+        )
+
+        MAX_WORDS = 500
+        import re
+
+        res = len(re.findall(r"\w+", doc))
+        if res > MAX_WORDS:
+            st.warning(
+                "⚠️ Your text contains "
+                + str(res)
+                + " words."
+                + " Only the first 500 words will be reviewed. Stay tuned as increased allowance is coming! 😊"
+            )
+
+            doc = doc[:MAX_WORDS]
+
+        submit_button = st.form_submit_button(label="✨ Donnez moi les topics!")
+
+
+st.markdown("## **🎈 Check & download results **")
+
+st.header("")
+
+cs, c1, c2, c3, cLast = st.columns([2, 1.5, 1.5, 1.5, 2])
+
+
+st.header("")
+
+
+
+
+
+
+c1, c2, c3 = st.columns([1, 3, 1])
+
+format_dictionary = {
+    "Relevancy": "{:.1%}",
+}
+
+
+
